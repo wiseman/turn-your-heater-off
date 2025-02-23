2cbc03da09eef2caca83a2f84b73b9a8e3f15c30
John Wiseman <jjwiseman@gmail.com>
2025-02-22 18:28:39 -0800

    Switch back to BrowserRouter which is more compatible with plausible.

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from 'virtual:generated-pages-react';
 import Layout from './components/layout';
 import './index.css';
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   routes.map((route) => ({
     ...route,
     element: <Layout>{route.element}</Layout>,
-  })),
-  {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true,
-    },
+  })), {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true
   }
-);
+});
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-} else {
-  console.error('Root element not found');
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
